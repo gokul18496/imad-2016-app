1d139f334553f05691911e27a4d71730b05f0fb2
Gohula Krishnan <gokul18496@gmail.com>
2016-10-06 00:59:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,6 @@
 console.log('Loaded!');
+delay(2000);
+var element = document.getElementById('main-text');
+element.innerHTML = `Hi! Some New Text.
+                    Cheap Thrills !!!
+                    Cold Water !!!`
