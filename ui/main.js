ee76c54971f2f55b6c5d7d80bd3fca74a448a40c
Gohula Krishnan <gokul18496@gmail.com>
2016-10-06 01:12:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 
-setTimeout(changeMainText, 2000);
+setTimeout(changeMainText, 1000);
 
 function changeMainText() {
     var element = document.getElementById('main-text');
@@ -8,3 +8,25 @@ function changeMainText() {
                         Cheap Thrills !!!
                         Cold Water !!!`
 }
+
+var pos = 0;
+var dir = 1;
+var img = document.getElementById('madi');
+img.onclick = function(){
+    if(pos === 0 && dir === 1){
+        img.style.marginLeft = "200px";
+        pos = 1;
+    } else if(pos === 1 && dir === 1){
+        img.style.marginLeft = "0px";
+        pos = 0;
+        dir = 0;
+    } else if(pos === 0 && dir === 0) {
+        img.style.marginLeft = "-200px";
+        pos = -1;
+        dir = 0;
+    } else if(pos === -1 && dir === 0) {
+        img.style.marginLeft = "0px";
+        pos = 0;
+        dir = 1;
+    }
+};
