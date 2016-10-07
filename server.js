a4ba0569efca4e0b8a6b440306206de757cd06ab
Gohula Krishnan <gokul18496@gmail.com>
2016-10-07 12:31:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get('/counter', function(req, res){
    res.send(counter.toString());
 });
 
-var names = '';
+var names = [];
 app.get('/submit-name/:name', function(req, res){
    var name = req.params.name;
    names.push(name);
