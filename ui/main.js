d10832b1d0798f5411b2283323ec39db546f3ad4
Gohula Krishnan <gokul18496@gmail.com>
2016-10-07 12:21:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,6 +18,21 @@ button.onclick = function(){
   request.send(null);
 };
 
+//Email Search
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit');
+submit.onclick = function() {
+  var names = ['name1', 'name2', 'name3'];
+  var list = "";
+  for(var i = 0; i < names.length; i++){
+      list += '<li>' + names[i] + '</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML(list);
+};
+
+
 /*
 console.log('Loaded!');
 
